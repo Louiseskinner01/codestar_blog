cabf09a96eb6bd9cb22368ccdce3be8664feacdb
Louise sKinner <louise.skinner40@gmail.com>
2025-11-16 21:21:08 +0000

    Create and connect PostgreSQL database

---
+++ env.py
@@ -0,0 +1,4 @@
+import os 
+
+os.environ.setdefault(
+    "DATABASE_URL", "postgresql://neondb_owner:npg_XvKJAh8kU4OH@ep-billowing-wave-agofms4f.c-2.eu-central-1.aws.neon.tech/coke_muse_tag_619327")
